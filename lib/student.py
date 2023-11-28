621265f9393af68f607a722025206ea009b91562
John Komba <john.mwangi1@student.moringaschool.com>
2023-11-28 12:17:44 +0300

    done with assignment

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from user import User
+from user import User  # Import the User class
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
